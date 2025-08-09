ff9234e5c446101d9376a8320b05ccca73eb2e64
Arbaz Khan <arbazkhan@Arbazs-MacBook-Air.local>
2025-08-09 13:49:52 +0530

    update vite file

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb', '**/*.gltf'],
+  build: {
+    emptyOutDir: false, // Don't remove existing dist folder content, only update
+  },
 })
