d1f8243fd99dc098a3bc894c3f640ab86b9f4ff2
Arbaz <arbaz@Arbazs-MacBook-Air.local>
2025-12-02 14:42:59 +0530

    logo updated

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,23 +4,11 @@ import './Logo.css';
 export default function Logo({ size = 'medium' }) {
     return (
         <div className={`logo-container logo-${size}`}>
-            <div className="logo-wrapper">
-                <div className="logo-brackets">
-                    <span className="bracket-left">&lt;</span>
-                    <div className="logo-content">
-                        <span className="logo-slash">/</span>
-                        <span className="logo-text">ARBAZ</span>
-                        <span className="logo-slash">/</span>
-                    </div>
-                    <span className="bracket-right">&gt;</span>
-                </div>
-                <div className="logo-underline"></div>
-            </div>
-            <div className="logo-particles">
-                {[...Array(6)].map((_, i) => (
-                    <div key={i} className={`particle particle-${i + 1}`} />
-                ))}
-            </div>
+            <img
+                src="/img/logo.png"
+                alt="Arbaz Portfolio Logo"
+                className="logo-image"
+            />
         </div>
     );
 }
